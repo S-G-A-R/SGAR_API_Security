6869f3971ad891f3beda5b94463a0c7b7662fe06
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-11-06 07:14:57 -0600

    Se avanzo en el ednpoint de crear organizacion

---
+++ SGAR_Seguridad/Properties/EndPoints/OrganizacionEndPoints.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.OpenApi.Models;
+using SGAR_Seguridad.Properties.DTOs;
+using SGAR_Seguridad.Properties.Services.Users;
+
+namespace SGAR_Seguridad.Properties.EndPoints
+{
+    public static class OrganizacionEndPoints
+    {
+        public static void add(this IEndpointRouteBuilder router)
+        {
+            var group = router.MapGroup("/organization").WithTags("Organizaciones");
+
+            //EndPoint para crear nuevo registro de usuarios
+            group.MapPost("/", async (UserRequest user, IUserServices userService) =>
+            {
+                if (user == null)
+                    return Results.BadRequest();
+
+                var id = await userService.PostUser(user);
+
+                return Results.Created($"/api/organization/{id}", new
+                {
+                    // Mensaje de éxito explícito
+                    message = "¡Organizacion creado exitosamente!",
+                    Id = id
+                });
+
+            }).WithOpenApi(o => new OpenApiOperation(o)
+            {
+                Summary = "Crear nueva Organizacion",
+                Description = "Crea un nueva organizacion en el sistema",
+            });//.RequireAuthorization(new AuthorizeAttribute { Roles = "Administrador" });
+        }
+    }
+}

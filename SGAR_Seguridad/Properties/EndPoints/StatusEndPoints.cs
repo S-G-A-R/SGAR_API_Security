116bfd0e36ebb781ae59396aa4dded1b8ee75585
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-11-05 20:28:36 -0600

    Se agrego un endpoint de estado

---
+++ SGAR_Seguridad/Properties/EndPoints/StatusEndPoints.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace SGAR_Seguridad.Properties.EndPoints
+{
+    public static class StatusEndPoints
+    {
+        public static void add(this IEndpointRouteBuilder router)
+        {
+            var group = router.MapGroup("/status").WithTags("Status");
+
+            group.MapGet("/status", async () =>
+            {
+                return Results.Ok(new { status = "API SGAR_Seguridad esta activa" });
+
+            }).WithOpenApi(o => new OpenApiOperation(o)
+            {
+                Summary = "Verificar el estado de la API",
+                Description = "Este endpoint permite verificar si la API de SGAR_Seguridad está activa y funcionando correctamente."
+            });
+        }
+    }
+}

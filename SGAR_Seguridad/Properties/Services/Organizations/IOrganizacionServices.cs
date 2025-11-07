6715713d0474f1136c784331a5e33f2ea13f2e6e
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-11-07 10:04:18 -0600

    Se trabajo y avanzo en los endpoints y servicios de organizacion.

---
+++ SGAR_Seguridad/Properties/Services/Organizations/IOrganizacionServices.cs
@@ -0,0 +1,15 @@
+﻿using SGAR_Seguridad.Properties.DTOs;
+
+namespace SGAR_Seguridad.Properties.Services.Organizations
+{
+    public interface IOrganizacionServices
+    {
+        Task<PaginatedResponseOrganization<OrganizationResponse>> GetOrganizations(int pageNumber = 1, int pageSize = 10);
+        Task<int> PostOrganization(OrganizationRequest organization);
+        Task<OrganizationResponse> GetOrganization(int organizationId);
+        //Task<int> PutUser(int userlId, UserRequest user);
+        Task<int> DeleteOrganization(int organizationId);
+        Task<CredencialesOrganizationResponse> Login(CredencialesOrganizationRequest orgUser);
+        //Task<UserResponse> BuscarPersonal(string? nombre, string? apellido, string? telefono, string? correo);
+    }
+}

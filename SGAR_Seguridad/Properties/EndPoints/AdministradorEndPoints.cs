83299d80920bc67e501ca2d29f60e491907ce201
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-11-07 10:06:41 -0600

    Se trabajo y avanzo en los endpoints y servicios de Administrador.

---
+++ SGAR_Seguridad/Properties/EndPoints/AdministradorEndPoints.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.OpenApi.Models;
+using SGAR_Seguridad.Properties.DTOs;
+using SGAR_Seguridad.Properties.Services.Administradores;
+
+namespace SGAR_Seguridad.Properties.EndPoints
+{
+    public static class AdministradorEndPoints
+    {
+        public static void add(this IEndpointRouteBuilder router)
+        {
+            var group = router.MapGroup("api/administrador").WithTags("Administraodores");
+
+            //EndPoint para obtener lista los registros de administradores paginados
+            group.MapGet("/list", async (int? page, int? pageSize, IAdministradorServices adminService) =>
+            {
+                var currentPage = page ?? 1;
+                var size = pageSize ?? 10;
+
+                if (currentPage < 1) currentPage = 1;
+                if (size < 1) size = 10;
+                if (size > 50) size = 50; // Límite máximo de registros por página
+
+                var admin = await adminService.GetAdministradores(currentPage, size);
+                return Results.Ok(admin);
+
+            }).WithOpenApi(o => new OpenApiOperation(o)
+            {
+                Summary = "Obtener lista de administrador paginada",
+                Description = "Retorna una lista paginada de administrador. Por defecto 10 registros por página.",
+            });//.RequireAuthorization(new AuthorizeAttribute { Roles = "Administrador" });
+
+            //EndPoint para obtener organizacion por id
+            group.MapGet("/{id}", async (int id, IAdministradorServices adminService) =>
+            {
+                var adminget = await adminService.GetAdministrador(id);
+                if (adminget == null)
+                    return Results.NotFound();
+                return Results.Ok(adminget);
+
+            }).WithOpenApi(o => new OpenApiOperation(o)
+            {
+                Summary = "Obtener un administrador por ID",
+                Description = "Obtiene un administrador específico mediante su ID",
+            });//.RequireAuthorization(new AuthorizeAttribute { Roles = "Administrador" });
+
+            //EndPoint para crear nuevo registro de organizaciones
+            group.MapPost("/", async (AdministradorRequest admin, IAdministradorServices adminService) =>
+            {
+                if (admin == null)
+                    return Results.BadRequest();
+
+                var id = await adminService.PostAdministrador(admin);
+
+                return Results.Created($"/api/administrador/{id}", new
+                {
+                    // Mensaje de éxito explícito
+                    message = "¡Administrador creado exitosamente!",
+                    Id = id
+                });
+
+            }).WithOpenApi(o => new OpenApiOperation(o)
+            {
+                Summary = "Crear nuevo administrador",
+                Description = "Crea un nuevo Administrador en el sistema",
+            });//.RequireAuthorization(new AuthorizeAttribute { Roles = "Administrador" });
+        }
+    }
+}
---
+++ SGAR_Seguridad/Properties/Services/Administradores/IAdministradorServices.cs
@@ -0,0 +1,12 @@
+﻿using SGAR_Seguridad.Properties.DTOs;
+
+namespace SGAR_Seguridad.Properties.Services.Administradores
+{
+    public interface IAdministradorServices
+    {
+        Task<PaginatedResponseAdmin<AdministradorResponse>> GetAdministradores(int pageNumber = 1, int pageSize = 10);
+        Task<int> PostAdministrador(AdministradorRequest administrador);
+        Task<int> DeleteAdministrador(int administradorId);
+        Task<AdministradorResponse> GetAdministrador(int administradorId);
+    }
+}

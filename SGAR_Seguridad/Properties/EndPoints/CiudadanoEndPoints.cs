bd220bd67798e75eb6162cbcf5c105106fbb8552
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-11-07 10:05:08 -0600

    Se avanzo y trabajo en los endpoints y servicios de ciudadano.

---
+++ SGAR_Seguridad/Properties/EndPoints/CiudadanoEndPoints.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.OpenApi.Models;
+using SGAR_Seguridad.Properties.DTOs;
+using SGAR_Seguridad.Properties.Services.Ciudadanos;
+using SGAR_Seguridad.Properties.Services.Organizations;
+
+namespace SGAR_Seguridad.Properties.EndPoints
+{
+    public static class CiudadanoEndPoints
+    {
+        public static void add(this IEndpointRouteBuilder router)
+        {
+            var group = router.MapGroup("api/ciudadano").WithTags("Ciudadanos");
+
+            //EndPoint para obtener lista los registros de ciudadanos paginados
+            group.MapGet("/list", async (int? page, int? pageSize, ICiudadanoServices ciudadanoService) =>
+            {
+                var currentPage = page ?? 1;
+                var size = pageSize ?? 10;
+
+                if (currentPage < 1) currentPage = 1;
+                if (size < 1) size = 10;
+                if (size > 50) size = 50; // Límite máximo de registros por página
+
+                var ciudadano = await ciudadanoService.GetCiudadanos(currentPage, size);
+                return Results.Ok(ciudadano);
+
+            }).WithOpenApi(o => new OpenApiOperation(o)
+            {
+                Summary = "Obtener lista de ciudadano paginada",
+                Description = "Retorna una lista paginada de ciudadno. Por defecto 10 registros por página.",
+            });//.RequireAuthorization(new AuthorizeAttribute { Roles = "Administrador" });
+
+            //EndPoint para obtener organizacion por id
+            group.MapGet("/{id}", async (int id, ICiudadanoServices ciudadanoService) =>
+            {
+                var getorg = await ciudadanoService.GetCiudadano(id);
+                if (getorg == null)
+                    return Results.NotFound();
+                return Results.Ok(getorg);
+
+            }).WithOpenApi(o => new OpenApiOperation(o)
+            {
+                Summary = "Obtener un ciudadano por ID",
+                Description = "Obtiene un ciudadano específico mediante su ID",
+            });//.RequireAuthorization(new AuthorizeAttribute { Roles = "Administrador" });
+
+            //EndPoint para crear nuevo registro de organizaciones
+            group.MapPost("/", async (CiudadanoRequest ciudadano, ICiudadanoServices ciudadanoService) =>
+            {
+                if (ciudadano == null)
+                    return Results.BadRequest();
+
+                var id = await ciudadanoService.PostCiudadano(ciudadano);
+
+                return Results.Created($"/api/ciudadano/{id}", new
+                {
+                    // Mensaje de éxito explícito
+                    message = "¡Ciudadano creado exitosamente!",
+                    Id = id
+                });
+
+            }).WithOpenApi(o => new OpenApiOperation(o)
+            {
+                Summary = "Crear nuevo ciudadano",
+                Description = "Crea un nuevo ciudadano en el sistema",
+            });//.RequireAuthorization(new AuthorizeAttribute { Roles = "Administrador" });
+
+            // EndPoint para eliminar un registro de ciudadano
+            group.MapDelete("/{id}", async (int id, ICiudadanoServices ciudadanoService) =>
+            {
+                var result = await ciudadanoService.DeleteCiudadano(id);
+                if (result == -1)
+                    return Results.NotFound(new
+                    {
+                        message = "No se encontró la ciudadano con el ID proporcionado."
+                    });
+                else
+                    return Results.Ok(new
+                    {
+                        // Mensaje de éxito explícito
+                        message = "¡Ciudadano eliminado exitosamente!",
+                        id = id
+                    });
+
+            }).WithOpenApi(o => new OpenApiOperation(o)
+            {
+                Summary = "Eliminar ciudadano",
+                Description = "Elimina una ciudadano existente mediante su ID",
+            });
+        }
+    }
+
+}
---
+++ SGAR_Seguridad/Properties/Services/Ciudadanos/ICiudadanoServices.cs
@@ -0,0 +1,14 @@
+﻿using SGAR_Seguridad.Properties.DTOs;
+
+namespace SGAR_Seguridad.Properties.Services.Ciudadanos
+{
+    public interface ICiudadanoServices
+    {
+        Task<PaginatedResponseCiudadano<CiudadanoResponse>> GetCiudadanos(int pageNumber = 1, int pageSize = 10);
+        Task<int> PostCiudadano(CiudadanoRequest ciudadano);
+        Task<CiudadanoResponse> GetCiudadano(int ciudadanoId);
+        //Task<int> PutUser(int userlId, UserRequest user);
+        Task<int> DeleteCiudadano(int ciudadanoId);
+        //Task<UserResponse> BuscarPersonal(string? nombre, string? apellido, string? telefono, string? correo);
+    }
+}

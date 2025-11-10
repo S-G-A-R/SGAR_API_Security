a7bf18010d059799a1e171510a9b88694b126fd5
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-11-10 07:28:37 -0600

    se agrego DTO para solicitud

---
+++ SGAR_Seguridad/Properties/DTOs/SolicitudDTO.cs
@@ -0,0 +1,42 @@
+﻿namespace SGAR_Seguridad.Properties.DTOs
+{
+    public class PaginatedResponseSolicitud<T>
+    {
+        public List<T> Items { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalCount { get; set; }
+        public bool HasNextPage { get; set; }
+        public bool HasPreviousPage { get; set; }
+    }
+
+
+    public class SolicitudRequest
+    {
+        public int Id { get; set; }
+
+        public int IdCiudadano { get; set; }
+
+        public int IdOrganizacion { get; set; }
+
+        public DateTime FechaSolicitud { get; set; }
+
+        public int Estado { get; set; }
+
+    }
+
+    public class SolicitudResponse
+    {
+        public int Id { get; set; }
+
+        public int IdCiudadano { get; set; }
+
+        public int IdOrganizacion { get; set; }
+
+        public DateTime FechaSolicitud { get; set; }
+
+        public int Estado { get; set; } 
+
+    }
+}
